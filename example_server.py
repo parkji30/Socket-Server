cf46a99b16e82bf85f5dde21e0ac1047d7658fb1
James Park <jinsoo.park@mail.utoronto.ca>
2019-01-21 23:13:28 -0500

    Add files via upload

---
+++ example_server.py
@@ -0,0 +1,17 @@
+import os
+os.chdir("/home/labuser/Desktop/googledrive/code/Samarium_control/Widgets/zmq_sockets")  
+from zmq_server_socket import zmq_server_socket
+import zmq
+import time
+import random
+
+publisher = zmq_server_socket(5560, "example")
+
+while True:
+    he_ra = 75 + random.randint(-10,10)
+    ni_ra = 275 + random.randint(-10, 10)   
+    data_dict = {'HeTemp' : he_ra, 'NiTemp' : ni_ra}
+    publisher.send(data_dict) 
+    publisher.print_current_data()   
+    time.sleep(0.1)                     #
+publisher.close()
